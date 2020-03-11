5f09ac6fe77b24191bf3ba3b2e31d3d753b0c147
danhhuynh1998 <1610389@hcmut.edu.vn>
2020-03-11 22:44:22 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,113 @@
+const express = require("express");
+const chalk = require("chalk");
+const socketIO = require("socket.io");
+const port = 3700;
+
+const {
+    AttributeIds,
+    OPCUAClient,
+    TimestampsToReturn,
+} = require("node-opcua");
+
+
+const hostname = require("os").hostname().toLowerCase();
+// const endpointUrl = "opc.tcp://" + hostname + ":26543/UA/SampleServer";
+const endpointUrl = "opc.tcp://DESKTOP-B8N6V6G:4840";
+const nodeIdToMonitor = "ns=1;s=\"Device_1\".\"Variable_1\"";
+
+(async () => {
+    try {
+        /////////////////////////////////
+        const client = OPCUAClient.create({
+            endpoint_must_exist: false
+        });
+        client.on("backoff", (retry, delay) => {
+            console.log("Retrying to connect to ", endpointUrl, " attempt ", retry);
+        });
+        console.log(" connecting to ", chalk.cyan(endpointUrl));
+        await client.connect(endpointUrl);
+        console.log(" connected to ", chalk.cyan(endpointUrl));
+        //////////////////////////////////
+
+        const session = await client.createSession();
+        console.log(" session created".yellow);
+
+        const subscription = await session.createSubscription2({
+            requestedPublishingInterval: 2000,
+            requestedMaxKeepAliveCount: 20,
+            requestedLifetimeCount: 6000,
+            maxNotificationsPerPublish: 1000,
+            publishingEnabled: true,
+            priority: 10
+        });
+
+        subscription.on("keepalive", function () {
+            console.log("keepalive");
+        }).on("terminated", function () {
+            console.log(" TERMINATED ------------------------------>")
+        });
+
+        // --------------------------------------------------------
+        const app = express();
+        app.get("/", function (req, res) {
+            res.send("It works! <a href='./index.html'>Start Here</a>");
+        });
+
+        app.use(express.static(__dirname + '/'));
+
+        const io = socketIO.listen(app.listen(port));
+
+        io.sockets.on('connection', function (socket) {
+        });
+
+        console.log("Listening on port " + port);
+        console.log("visit http://localhost:" + port);
+        // --------------------------------------------------------
+
+        const itemToMonitor = {
+            nodeId: nodeIdToMonitor,
+            attributeId: AttributeIds.Value
+        };
+        const parameters = {
+            samplingInterval: 100,
+            discardOldest: true,
+            queueSize: 100
+        };
+        const monitoredItem = await subscription.monitor(itemToMonitor, parameters, TimestampsToReturn.Both);
+
+        monitoredItem.on("changed", (dataValue) => {
+            console.log(dataValue.value.toString());
+            io.sockets.emit('message', {
+                value: dataValue.value.value,
+                timestamp: dataValue.serverTimestamp,
+                nodeId: nodeIdToMonitor,
+                browseName: "Temperature"
+            });
+        });
+
+        // detect CTRL+C and close
+        let running = true;
+        process.on("SIGINT", async () => {
+            if (!running) {
+                return; // avoid calling shutdown twice
+            }
+            console.log("shutting down client");
+            running = false;
+
+            await subscription.terminate();
+
+            await session.close();
+            await client.disconnect();
+            console.log("Done");
+            process.exit(0);
+
+        });
+
+    }
+    catch (err) {
+        console.log(chalk.bgRed.white("Error" + err.message));
+        console.log(err);
+        process.exit(-1);
+    }
+})();
+
